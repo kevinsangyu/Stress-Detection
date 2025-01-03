5a3a8ab9ce012f88b65535ea2131e91bd492b7b2
Kevin <kevin.sang.yu@gmail.com>
2025-01-03 17:56:12 +1100

    Redid clasp using claspy instead of sktime, worked quite well when only using HR, HR_delta.

--- segmentation.py
+++ segmentation.py
@@ -1,67 +1,51 @@
 import matplotlib.pyplot as plt
-from sktime.annotation.clasp import ClaSPSegmentation
+from claspy.segmentation import BinaryClaSPSegmentation
 from sktime.annotation.plotting.utils import plot_time_series_with_change_points, plot_time_series_with_profiles
 from sktime.datasets import load_electric_devices_segmentation
 import ruptures as rpt
 import extractData
+import pandas as pd
 
 
 class CLASP(object):
     def __init__(self, method="sampling", delta_switch=True):
-        self.data = extractData.Extract(r"data/Kevin data/2024_04_22_vs_pegasus")
-        homogenise_method = method
-        delta = delta_switch
-        if delta:
-            self.data.delta()
-        self.data.homogenise(method=homogenise_method, size=10)
+        self.data = extractData.Extract(r"data/Kevin data/2024_04_29_vs_snortsnort")
+        self.data.delta()
+        self.data.homogenise(method="window", size=10)
+        self.data.combine_df()
+        self.df = self.data.df
+        print(f"Available features: {self.df.columns}")
 
-        self.ts = self.data.HR.df
-        if homogenise_method == "window":
-            if delta:
-                self.ts = self.ts['MA_delta']
-            else:
-                self.ts = self.ts['MA']
+    def segment(self, feature="HR", exclude="no feature should have this string.!@#)(*%"):
+        if not feature:
+            # use all features if no specific one is provided
+            ts = self.df
+            cols = [col for col in self.df.columns]
         else:
-            if delta:
-                self.ts = self.ts['delta']
-            else:
-                self.ts = self.ts[0]
+            # grab the MA, delta and raw features of given feature
+            # also works in grabbing just MA values or just delta values if you use 'MA' or 'delta'
+            cols = [col for col in self.df.columns if feature in col and exclude not in col]
+            print(f"Selected columns: {cols}")
+            ts = self.df[cols]
+        ts.dropna(inplace=True)
 
-    def segment(self):
-        n_cps = 5
-        period_length = 10  # ts.size // n_cps
-        print(f"n_cps: {n_cps}, period_length: {period_length}")
-        clasp = ClaSPSegmentation(period_length=period_length, n_cps=n_cps)
-        found_cps = clasp.fit_predict(self.ts)
-        print("The found change points are", found_cps.to_numpy())
+        clasp = BinaryClaSPSegmentation(n_segments=5, validation=None)
+        found_cps = clasp.fit_predict(time_series=ts.values)
+        print("The found change points are", found_cps)
 
         indices = [i for i, x in enumerate(self.data.TAGS) if x == 1]
-        print("True change points/tags")
-        _ = plot_time_series_with_profiles(
-            "Volleyball game",
-            self.ts,
-            clasp.profiles,
-            indices,
-            found_cps,
-        )
+        print("True change points/tags: ", indices)
+        clasp.plot(gt_cps=pd.Series(indices),
+                   heading=f"Segmentation of Volleyball game",
+                   ts_name=feature, font_size=18)
         plt.show()
 
     def test(self):
-        ts, period_size, true_cps = load_electric_devices_segmentation()
-        _ = plot_time_series_with_change_points("Electric Devices", ts, true_cps)
-        clasp = ClaSPSegmentation(period_length=period_size, n_cps=5)
-        found_cps = clasp.fit_predict(ts)
-        profiles = clasp.profiles
-        scores = clasp.scores
-        print("The found change points are", found_cps.to_numpy())
-        _ = plot_time_series_with_profiles(
-            "Electric Devices",
-            ts,
-            profiles,
-            true_cps,
-            found_cps,
-        )
-        plt.show()
+        from claspy.data_loader import load_tssb_dataset
+        dataset, window_size, true_cps, time_series = load_tssb_dataset(names=("CricketX",)).iloc[0, :]
+        clasp = BinaryClaSPSegmentation()
+        clasp.fit_predict(time_series)
+        print(clasp.window_size)
 
 
 class RUPTURES(object):
@@ -88,7 +72,7 @@ class RUPTURES(object):
 
         # select model and algorithm then predict
         model = "rbf"
-        algo = rpt.Binseg(model=model, min_size=10).fit(ts.values)
+        algo = rpt.Dynp(model=model, min_size=10).fit(ts.values)
         print("Segmenting...")
         predicted = algo.predict(n_bkps=6)
 
@@ -118,7 +102,8 @@ class RUPTURES(object):
 
 
 if __name__ == '__main__':
-    rup = RUPTURES()
-    rup.segment(feature="delta", exclude="MA")
-    # clsp = CLASP(delta_switch=True)
-    # clsp.segment()
+    # rup = RUPTURES()
+    # rup.segment(feature="")
+    clsp = CLASP()
+    # clsp.test()
+    clsp.segment(feature="EDA", exclude="MA")
