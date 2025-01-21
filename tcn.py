7094feddd881ce3d9ba39ddf605f90ca1c04bada
Kevin <74086991+kevinsangyu@users.noreply.github.com>
2025-01-21 21:54:01 +1100

    Finalising TCN for report.

--- tcn.py
+++ tcn.py
@@ -1,11 +1,12 @@
 import pandas as pd
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, precision_score
 from sklearn.utils.class_weight import compute_class_weight
 import extractData
 import torch
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class TCN:
@@ -18,6 +19,12 @@ class TCN:
         # init fields for later
         self.X_train = self.X_test = self.y_train = self.y_test = None  # dataframe into torch tensors
         self.model = self.criterion = self.optim = None  # neural network model, criterion and optimiser (respectively)
+
+        np.random.seed(42)
+        torch.manual_seed(42)
+        torch.cuda.manual_seed_all(42)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
 
     def drop(self, drop_list=()):
         if drop_list:
@@ -64,12 +71,16 @@ class TCN:
         self.y_train = torch.tensor(self.y_train, dtype=torch.float32)
         self.y_test = torch.tensor(self.y_test, dtype=torch.float32)
 
-    def train(self, epochs, batch_size):
+    def train(self, epochs, batch_size, weighted=False, custom_weight=()):
         self.model.train()
-        class_weights = compute_class_weight('balanced', classes=np.array([0.0, 1.0]), y=self.y_train.numpy().flatten())
-        class_weights = torch.tensor(class_weights, dtype=torch.float32)
-
-        self.criterion = torch.nn.BCEWithLogitsLoss(pos_weight=torch.tensor(class_weights[1] / class_weights[0]))
+        if weighted:
+            if custom_weight:
+                class_weights = custom_weight
+            else:
+                class_weights = compute_class_weight('balanced', classes=np.array([0.0, 1.0]), y=self.y_train.numpy().flatten())
+            class_weights = torch.tensor(class_weights, dtype=torch.float32)
+            print(f"Class weights: {class_weights}")
+            self.criterion = torch.nn.BCEWithLogitsLoss(pos_weight=torch.tensor(class_weights[1] / class_weights[0]))
         for epoch in range(epochs):
             permutation = torch.randperm(self.X_train.size(0))
             epoch_loss = 0
@@ -93,6 +104,7 @@ class TCN:
             predictions = (outputs > 0.5).float()
         # print(predictions)
         print(classification_report(self.y_test.numpy(), predictions.numpy()))
+        return predictions
 
 
 class TCNBlock(torch.nn.Module):
@@ -147,10 +159,81 @@ class TCNModule(torch.nn.Module):
         return out
 
 
+def calc_precision(epochs=20, plot=False, weight=20):
+    results = {}
+
+    all = TCN()
+    all.defineModel()
+    all.dataprep()
+    all.train(epochs=epochs, batch_size=32, weighted=True, custom_weight=(1, weight))
+    all_pred = all.evaluate()
+    results["all"] = precision_score(all.y_test, all_pred)
+
+    raw = TCN()
+    raw.drop(("MA", "delta"))
+    raw.defineModel()
+    raw.dataprep()
+    raw.train(epochs=epochs, batch_size=32, weighted=True, custom_weight=(1, weight))
+    raw_pred = raw.evaluate()
+    results["raw"] = precision_score(raw.y_test, raw_pred)
+
+    delta = TCN()
+    delta.select('delta')
+    delta.defineModel()
+    delta.dataprep()
+    delta.train(epochs=epochs, batch_size=32, weighted=True, custom_weight=(1, weight))
+    delta_pred = delta.evaluate()
+    results["delta"] = precision_score(delta.y_test, delta_pred)
+
+    MA = TCN()
+    MA.select('MA')
+    MA.defineModel()
+    MA.dataprep()
+    MA.train(epochs=epochs, batch_size=32, weighted=True, custom_weight=(1, weight))
+    MA_pred = MA.evaluate()
+    results["MA"] = precision_score(MA.y_test, MA_pred)
+
+    if plot:
+        plt.bar(range(len(results)), list(results.values()), align='center')
+        plt.xticks(range(len(results)), list(results.keys()))
+        plt.title("Precision scores of different data selections")
+        plt.ylabel("Precision Score")
+        plt.xlabel(f"Data selections, class_weight = {weight}, epochs = {epochs} for weighted selections")
+        plt.show()
+    return results
+
+
+def calc_epochs(limit):
+    indexes = []
+    epochs = []
+    for epoch in range(10, limit, 10):
+        results = calc_precision(plot=False, weight=50, epochs=epoch)
+        average_precision = sum(results.values()) / len(results.values())
+        indexes.append(epoch)
+        epochs.append(average_precision)
+    plt.title("Optimal epoch for precision")
+    plt.xlabel("Epochs")
+    plt.ylabel("Precision Score")
+    plt.plot(indexes, epochs)
+    plt.show()
+
+
+def custom_weights(limit):
+    indexes = []
+    weights = []
+    for weight in range(10, limit, 10):
+        results = calc_precision(plot=False, weight=weight, epochs=20)
+        average_precision = sum(results.values()) / len(results.values())
+        indexes.append(weight)
+        weights.append(average_precision)
+    plt.title("Optimal class weight for precision")
+    plt.xlabel("Class weights")
+    plt.ylabel("Precision Score")
+    plt.plot(indexes, weights)
+    plt.show()
+
+
 if __name__ == '__main__':
-    tcn = TCN()
-    tcn.drop(("MA", "delta"))
-    tcn.defineModel()
-    tcn.dataprep()
-    tcn.train(epochs=20, batch_size=32)
-    tcn.evaluate()
+    calc_precision(epochs=30, plot=True, weight=50)
+    # custom_weights(100)
+    # calc_epochs(50)
